2182abaf012e71088737f46b1e7b86a5da656e70
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-02 14:17:59 -0500

    test: Add ThreadBundle start function test

--- tests/test_thread_bundle.py
+++ tests/test_thread_bundle.py
@@ -1,10 +1,11 @@
 import threading
+import time
 import unittest
 from ezsignal.ThreadBundle import ThreadBundle
 
 
 def target_func():
-    pass
+    time.sleep(.5)
 
 
 class test_thread_bundle(unittest.TestCase):
@@ -21,6 +22,18 @@ class test_thread_bundle(unittest.TestCase):
         bundle.add(thread)
         self.assertEqual(len(bundle.threads), 1)
         self.assertEqual(bundle.threads[0], thread)
+
+    def test_thread_bundle_start(self):
+        bundle = ThreadBundle()
+        thread = threading.Thread(target=target_func)
+        bundle.add(thread)
+        bundle.start()
+        self.assertTrue(bundle.is_running)
+        self.assertFalse(bundle.is_dead)
+        thread.join()
+        self.assertTrue(bundle.is_dead)
+        self.assertFalse(bundle.is_running)
+
 
 
 if __name__ == '__main__':

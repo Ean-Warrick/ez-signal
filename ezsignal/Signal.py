955a94c7698aaefe871b8d8edc74e4341a485643
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-02 15:20:41 -0500

    feature: Add connect function to Signal class

--- ezsignal/Signal.py
+++ ezsignal/Signal.py
@@ -1,3 +1,11 @@
+from ezsignal import Connection
+
+
 class Signal:
     def __init__(self):
         self.connections = []
+
+    def connect(self, func):
+        connection = Connection(func)
+        self.connections.append(connection)
+        return connection

ffd02b485d94eeb2e5fab4f3283b2671571ac82a
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-02 14:46:48 -0500

    feature: add ThreadBundle class function start and _dead

--- ezsignal/ThreadBundle.py
+++ ezsignal/ThreadBundle.py
@@ -1,8 +1,25 @@
+import threading
+
+
 class ThreadBundle:
     def __init__(self):
         self.threads = []
         self.is_running = False
         self.is_dead = False
 
+    def _death(self):
+        for thread in self.threads:
+            thread.join()
+        self.is_running = False
+        self.is_dead = True
+
     def add(self, thread):
         self.threads.append(thread)
+
+    def start(self):
+        death_thread = threading.Thread(target=self._death)
+        self.is_running = True
+        for thread in self.threads:
+            thread.start()
+        death_thread.start()
+

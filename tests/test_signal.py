633b5f025f3ca843a00e086f7e71a4551cf9253c
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-02 15:08:55 -0500

    test: Added Signal class init test

---
+++ tests/test_signal.py
@@ -0,0 +1,17 @@
+import unittest
+from ezsignal import Connection
+
+
+def test_connection_func():
+    pass
+
+
+class test_signal(unittest.TestCase):
+
+    def test_signal_init(self):
+        signal = Signal()
+        self.assertEqual(len(signal.connections), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()

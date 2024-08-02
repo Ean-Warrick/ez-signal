51ee7a3e6e61dfb089853617deab5eca2dd0326f
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-02 13:24:11 -0500

    feat: Add disconnect function in Connection class

--- ezsignal/Connection.py
+++ ezsignal/Connection.py
@@ -2,3 +2,7 @@ class Connection:
     def __init__(self, func):
         self.is_connected = True
         self.func = func
+
+    def disconnect(self):
+        self.is_connected = False
+        self.func = None

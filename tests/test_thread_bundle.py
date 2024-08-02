7a616c0cc02d50b0dd2ca5a0203530ce60e19a76
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-02 13:42:51 -0500

    test: Add thread bundle init test

---
+++ tests/test_thread_bundle.py
@@ -0,0 +1,13 @@
+import unittest
+
+class test_thread_bundle(unittest.TestCase):
+
+    def test_thread_bundle_init(self):
+        bundle = ThreadBundle()
+        self.assertEqual(len(bundle.threads), 0)
+        self.assertFalse(bundle.is_dead)
+        self.assertFalse(bundle.is_running)
+
+
+if __name__ == '__main__':
+    unittest.main()

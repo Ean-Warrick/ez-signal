e13f72445511613205392041f0446b152ca39b6d
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-03 11:42:18 -0500

    feat!: Remove emit function from Signal class
    The emit function is now located in the Event class

--- ezsignal/Signal.py
+++ ezsignal/Signal.py
@@ -1,7 +1,5 @@
-import threading
-
 from ezsignal import Connection
-from ezsignal import ThreadBundle
+
 
 class Signal:
     def __init__(self):
@@ -11,18 +9,3 @@ class Signal:
         connection = Connection(func)
         self.connections.append(connection)
         return connection
-
-    def emit(self, *args):
-        thread_bundle = ThreadBundle()
-        remove_connections = []
-        for connection in self.connections:
-            if not connection.is_connected:
-                remove_connections.append(connection)
-            else:
-                thread = threading.Thread(target=connection.func, daemon=True, args=args)
-                thread_bundle.add(thread)
-        for connection in remove_connections:
-            self.connections.remove(connection)
-        thread_bundle.start()
-        return thread_bundle
-
--- tests/test_signal.py
+++ tests/test_signal.py
@@ -2,7 +2,6 @@ import time
 import unittest
 from ezsignal import Signal
 from ezsignal import Connection
-from ezsignal import ThreadBundle
 
 
 def test_connection_func():
@@ -21,18 +20,6 @@ class test_signal(unittest.TestCase):
         self.assertTrue(isinstance(connection, Connection))
         self.assertEqual(len(signal.connections), 1)
         self.assertEqual(signal.connections[0], connection)
-
-    def test_signal_emit(self):
-        signal = Signal()
-        connection = signal.connect(test_connection_func)
-        thread_bundle = signal.emit()
-        self.assertTrue(isinstance(thread_bundle, ThreadBundle))
-        self.assertEqual(len(thread_bundle.threads), 1)
-        self.assertTrue(thread_bundle.is_running)
-        self.assertFalse(thread_bundle.is_dead)
-        thread_bundle.join()
-        self.assertFalse(thread_bundle.is_running)
-        self.assertTrue(thread_bundle.is_dead)
 
 
 if __name__ == '__main__':

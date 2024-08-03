d6e67806a93067c2450fe55f4b29c36a166a702d
Ean-Warrick <ean.warrick@my.simpson.edu>
2024-08-03 11:10:10 -0500

    feature: Add Event class to ezsignal package

---
+++ ezsignal/Event.py
@@ -0,0 +1,6 @@
+from ezsignal.Signal import Signal
+
+
+class Event:
+    def __init__(self):
+        self.signal = Signal()
--- ezsignal/__init__.py
+++ ezsignal/__init__.py
@@ -1,3 +1,4 @@
 from .Connection import Connection
 from .ThreadBundle import ThreadBundle
 from .Signal import Signal
+from .Event import Event
